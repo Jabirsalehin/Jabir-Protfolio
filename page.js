d895381485946af09746e77916fbce21c456598c
Jabir salehin <muhammadjabirsalehin@gmail.com>
2026-04-06 23:47:37 -0700

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function ProjectPage({ params }) {
-  return (
-    <div className="min-h-screen bg-black text-white p-10">
-      <h1 className="text-3xl">{params.slug}</h1>
-      <p className="mt-4 text-gray-400">Detailed case study coming soon...</p>
-    </div>
-  );
-}
